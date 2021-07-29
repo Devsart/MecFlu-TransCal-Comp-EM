6dbf8e66e4aeee07c5923e8d7145c024c68b469b
DevSart <matheus.sartor@poli.ufrj.br>
2021-07-29 03:08:31 -0300

    plot com funcoes no topo

--- plotMalhas.py
+++ plotMalhas.py
@@ -4,6 +4,9 @@ Created on Thu Jul 22 08:08:41 2021
 
 @author: matheus.sartor
 """
+
+import os
+from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
@@ -47,26 +50,48 @@ def IENcreate(ne):
         
 
 
-def getXY(n_pointsx=4,n_pointsy=4,Lx = 1, Ly = 1, mode="square"):
+def getXY(n_pointsx=4,n_pointsy=4,Lx = 1, Ly = 1, mode="square",function="sin",coef=.2):
     x = []
     y = []
     for i in range(0,n_pointsy):
         for j in range(0,n_pointsx):
             x.append(j/(n_pointsx-1))
             y.append(i/(n_pointsy-1))
-    plt.plot(x,y,'ko')
     IEN = IEN2D(n_pointsx,n_pointsy,mode)
+    if(function=="sin"):
+        y[-n_pointsx::] += np.sin(np.multiply(x[:n_pointsx:],2*np.pi))*coef
     return x,y,IEN
 
 def plotMalhaQuadrada(X,Y,IEN):
+    name = 'Malha Ordenada'
+    Path(os.path.join('results', name)).mkdir(parents=True, exist_ok=True)
+    ne = IEN.shape[0]
+    crit_list = []
+    crit_list = []
+    for e in range(0,ne):
+        [v1,v2,v3] = IEN[e]
+        x1 = X[v1]; x2 = X[v2]; x3 = X[v3]
+        y1 = Y[v1]; y2 = Y[v2]; y3 = Y[v3]
+        a = np.sqrt((x3-x2)**2+(y3-y2)**2)
+        b = np.sqrt((x3-x1)**2+(y3-y1)**2)
+        c = np.sqrt((x2-x1)**2+(y2-y1)**2)
+        s = (a + b + c)/2
+        A = np.sqrt(s*(s-a)*(s-b)*(s-c)) # Formula de Heron
+        ha = 2*A/a
+        hb = 2*A/b
+        hc = 2*A/c
+        crit = ((ha*hb*hc)/(a*b*c))/((np.sqrt(3)/2)**3)
+        crit_list.append((0,0,.6,crit))
     xy = np.stack((X, Y), axis=-1)
     verts = xy[IEN]
     ax=plt.gca()
     pc = matplotlib.collections.PolyCollection(verts,edgecolors=('black',),
-                                                 facecolors='lightblue',
+                                                 facecolor='blue',
                                                  linewidths=(0.7,))
+    pc.set_color(crit_list)
     ax.add_collection(pc)
     plt.plot(X,Y,'ko')
+    plt.savefig(os.path.join('results', name, 'Malha_Ordenada.png'))
 
 def IEN2D(nx,ny, mode="square"):
     if(mode=="square"):
